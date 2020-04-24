bb0958c3bce07c8c6753d4b873134bb8db84ea10
rahmatulaina <rahmatulaina28@gmail.com>
2020-04-24 18:07:35 +0800

    Project IDEA

---
+++ Modul01/Modul01_IDEA/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        String namaDepan = "Rahmatul";
+        String namaBelakang = "Aina";
+        int usia = 18;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+
+
+        System.out.println(namaDepan + namaBelakang);
+    }
+}
